0b701c7b116b1af36b982e7cf0c705eb5858a465
Bret Johnson <bret@bretajohnson.com>
2025-07-01 03:56:25 -0400

    Add UI preview support for different game states
    Add preview support, allowing developers to quickly view and test the
    game in different states without having to play through to reach those
    states.

--- src/MainPage.cs
+++ src/MainPage.cs
@@ -1,4 +1,6 @@
-﻿using DrawnUi.Views;
+﻿using BreakoutGame.Game;
+using DrawnUi.Views;
+using PreviewFramework;
 
 namespace BreakoutGame
 {
@@ -41,5 +43,55 @@ namespace BreakoutGame
 
             this.Content = Canvas;
         }
+
+#if PREVIEWS
+        [Preview]
+        public static void Welcome() => ApplyPreviewState(new PreviewAppState() { GameState = GameState.Ready });
+
+        [Preview]
+        public static void Playing() => ApplyPreviewState(new PreviewAppState() { GameState = GameState.Playing });
+
+        [Preview]
+        public static void Paused() => ApplyPreviewState(new PreviewAppState() { GameState = GameState.Paused });
+
+        [Preview]
+        public static void Ended() => ApplyPreviewState(new PreviewAppState() { GameState = GameState.Ended });
+
+        [Preview]
+        public static void LevelComplete() => ApplyPreviewState(new PreviewAppState() { GameState = GameState.LevelComplete });
+
+        [Preview]
+        public static void DemoPlay() => ApplyPreviewState(new PreviewAppState() { GameState = GameState.DemoPlay });
+
+        [Preview]
+        public static void Level2() => ApplyPreviewState(PreviewAppState.BeginningOfLevel(2));
+
+        [Preview]
+        public static void Level3() => ApplyPreviewState(PreviewAppState.BeginningOfLevel(3));
+
+        [Preview]
+        public static void Level4() => ApplyPreviewState(PreviewAppState.BeginningOfLevel(4));
+
+        [Preview]
+        public static void Level5() => ApplyPreviewState(PreviewAppState.BeginningOfLevel(5));
+
+        [Preview]
+        public static void Level6() => ApplyPreviewState(PreviewAppState.BeginningOfLevel(6));
+
+        [Preview]
+        public static void Level7() => ApplyPreviewState(PreviewAppState.BeginningOfLevel(7));
+
+        [Preview]
+        public static void Level8() => ApplyPreviewState(PreviewAppState.BeginningOfLevel(8));
+
+
+        private static void ApplyPreviewState(PreviewAppState previewAppState)
+        {
+            var breakoutGame = Game.BreakoutGame.Instance ??
+                throw new InvalidOperationException("BreakoutGame isn't initialized");
+
+            breakoutGame.ApplyPreviewState(previewAppState);
+        }
+#endif
     }
 }
