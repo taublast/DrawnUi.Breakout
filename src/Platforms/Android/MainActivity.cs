4d884abc343a613e10ea6b7fd0c8d48e05c71026
Nick Kovalsky <taublast@gmail.com>
2025-11-07 13:52:09 +0300

    android LaunchMode.SingleTask

--- src/Platforms/Android/MainActivity.cs
+++ src/Platforms/Android/MainActivity.cs
@@ -5,13 +5,12 @@ using Android.OS;
 namespace Breakout;
 
 [Activity(Theme = "@style/MainTheme", MainLauncher = true, 
-    LaunchMode = LaunchMode.SingleTop, 
+    LaunchMode = LaunchMode.SingleTask, 
     ConfigurationChanges = ConfigChanges.ScreenSize | 
                            ConfigChanges.Orientation
                            | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | 
                            ConfigChanges.SmallestScreenSize | ConfigChanges.Density, 
     ScreenOrientation = ScreenOrientation.SensorPortrait)]
-
 public class MainActivity : MauiAppCompatActivity
 {
 }
