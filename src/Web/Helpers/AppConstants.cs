5183ed61cb193d3fc80ea597f0239cb89aed281f
AppoMobi <taublast@gmail.com>
2026-05-22 20:45:21 +0200

    fullscreen fix

--- src/Web/Helpers/AppConstants.cs
+++ src/Web/Helpers/AppConstants.cs
@@ -2,7 +2,7 @@ namespace Breakout.Helpers;
 
 public static class AppConstants
 {
-    public const string Version = "1.0.2.1";
+    public const string Version = "1.0.2.2";
 
     public const string LastSeenVersionKey = "app.version";
 }
